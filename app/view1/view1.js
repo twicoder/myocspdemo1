d73ebafb76a25fb8381a827dcb574c1f57e37259
twicoder <renqingwei123@163.com>
2018-04-27 09:22:46 +0800

    add all element types

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -30,33 +30,18 @@ angular.module('myApp.view1', ['ngRoute'])
   var canvasClicked = false;
   var panning = false;
 
-  // ocspspace.Canvas.init();
-  // ocspspace.Graph.init();
-  // ocspspace.Draggable.init();
-
-
   var currentNodeType = null;
-
 
   var canvasContainer = $('#canvas-container');
   var currentCanvasTop = canvasContainer[0].offsetTop;
   var currentCavansLeft = canvasContainer[0].offsetLeft;
 
 
-  var processorMap = d3.map();
+
+
+
   var svg = d3.select('#canvas-container').append('svg')
     .on('contextmenu', function () {
-      // // reset the canvas click flag
-      // canvasClicked = false;
-      //
-      // // since the context menu event propagated back to the canvas, clear the selection
-      // nf.CanvasUtils.getSelection().classed('selected', false);
-      //
-      // // show the context menu on the canvas
-      // nf.ContextMenu.show();
-      //
-      // // prevent default browser behavior
-      // d3.event.preventDefault();
     })
     .attr('width','100%')
     .attr('height','600px');
@@ -70,26 +55,148 @@ angular.module('myApp.view1', ['ngRoute'])
 
 
 
+  var processorMap = d3.map();
+  var allConnections = d3.map();
+  var allCanvasLines = d3.map();
+
+
 
   function dragCircleMove() {
+    var currObjId = $(this)[0].id;
+    // processorMap.get(currObjId).position.cx = d3.event.x;
+    // processorMap.get(currObjId).position.cy = d3.event.y;
+
+    processorMap.set(currObjId, {
+      'id':currObjId,
+      'position':{
+        'x':d3.event.x,
+        'y':d3.event.y,
+        'nodetype':processorMap.get(currObjId).position.nodetype
+      }
+    });
+
     d3.select(this)
       .attr("cx", $(this).cx = d3.event.x)
       .attr("cy", $(this).cy = d3.event.y);
+
+    refreshLines();
   }
 
   function dragSeocndMove(){
-    d3.select(this)
-      .attr('x',$(this).x = d3.event.x - 30)
-      .attr('y',$(this).y = d3.event.y - 40);
+    var currObjId = $(this)[0].id;
+
+    console.log('dragSeocndMove:');
+    console.log(currObjId);
+    if(processorMap.get(currObjId)){
+      console.log(processorMap.get(currObjId));
+      console.log('======');
+
+      processorMap.set(currObjId, {
+        'id':currObjId,
+        'position':{
+          'x':d3.event.x - 30,
+          'y':d3.event.y - 40,
+          'nodetype':processorMap.get(currObjId).position.nodetype
+        }
+      });
+
+
+      d3.select(this)
+        .attr('x',$(this).x = d3.event.x - 30)
+        .attr('y',$(this).y = d3.event.y - 40);
+
+      refreshLines();
+    }
+
+
   }
 
   function dragThirdMove(){
+    var currObjId = $(this)[0].id;
+
+    processorMap.set(currObjId, {
+      'id':currObjId,
+      'position':{
+        'x':d3.event.x - 30,
+        'y':d3.event.y - 40,
+        'nodetype':processorMap.get(currObjId).position.nodetype
+      }
+    });
+
+
     d3.select(this)
       .attr('x',$(this).x = d3.event.x - 30)
       .attr('y',$(this).y = d3.event.y - 40);
+
+    refreshLines();
+
   }
 
-  var dragCircle = d3.drag().on("drag", dragCircleMove);
+  var startNode = null;
+  var endNode = null;
+
+  var shouldDraw = false;
+  var currentStartCenter = null;
+  var currentEndCenter = null;
+  var tmpMovingLine = null;
+
+  function clickedOnGraphObj(graphObj){
+    if(!startNode){
+      startNode = graphObj;
+    } else if(!endNode){
+      endNode = graphObj;
+
+      // In this case, we should delete the tmpMovingLine and update all lines info
+      var connectionObj = {
+        'id':"line-id-"+new Date().getTime(),
+        'startid':startNode.id,
+        'endid':endNode.id
+      };
+
+      allConnections.set(connectionObj.id, connectionObj);
+
+      startNode = null;
+      endNode = null;
+
+      refreshLines();
+
+    } else {
+      console.log('Logic error....');
+    }
+  }
+
+  function refreshLines(){
+
+    console.log('refreshLines()...');
+    allCanvasLines.each(function(oldLine){
+      oldLine.remove();
+    });
+
+    allConnections.each(function(obj){
+      console.log(obj);
+
+      var currentObjLine = svg.append("line")
+        .attr("x1",processorMap.get(obj.startid).position.x)
+        .attr("y1",processorMap.get(obj.startid).position.y)
+        .attr("x2",processorMap.get(obj.endid).position.x)
+        .attr("y2",processorMap.get(obj.endid).position.y)
+        .attr("stroke","red")
+        .attr("stroke-width",2)
+        .attr("marker-start","url(#arrow)")
+        .attr("marker-end","url(#arrow)");
+
+      allCanvasLines.set(obj.id,currentObjLine);
+
+    });
+
+  }
+
+
+
+  var dragCircle = d3.drag().on("drag", dragCircleMove).on('end',function(){
+    // console.log('dropped!');
+    refreshLines();
+  });
   var dragSecond = d3.drag().on("drag", dragSeocndMove);
   var dragThird = d3.drag().on("drag", dragThirdMove);
 
@@ -97,7 +204,9 @@ angular.module('myApp.view1', ['ngRoute'])
   function addNode(x,y){
     // console.log("Add one node with x=",x,"y=",y);
     var newNode = ocspspace.Graph.getNode(x,y,currentNodeType);
-    processorMap.set(newNode.id,newNode.position);
+    processorMap.set(newNode.id,newNode);
+    console.log('new added node:');
+    console.log(newNode);
     drawNode(svg,processorMap.get(newNode.id));
 
     // ocspspace.Graph.add(newNode,false);
@@ -117,18 +226,66 @@ angular.module('myApp.view1', ['ngRoute'])
     console.log(d);
   }
 
+
+
+  d3.select('#canvas-container').on('mousemove',function($event){
+
+    if(shouldDraw){
+      if(tmpMovingLine){
+        tmpMovingLine.remove();
+      }
+      if(currentStartCenter){
+        tmpMovingLine = svg.append("line")
+          .attr("x1",currentStartCenter.x)
+          .attr("y1",currentStartCenter.y)
+          .attr("x2",d3.event.clientX - currentCavansLeft)
+          .attr("y2",d3.event.clientY - currentCanvasTop)
+          .attr("stroke","red")
+          .attr("stroke-width",2)
+          .attr("marker-start","url(#arrow)")
+          .attr("marker-end","url(#arrow)");
+      }
+
+
+    }
+
+  });
+
+  // function drawFinalLine(){
+  //   if(currentStartCenter && currentEndCenter){
+  //     if(tmpMovingLine){
+  //       tmpMovingLine.remove();
+  //     }
+  //
+  //     svg.append("line")
+  //       .attr("x1",currentStartCenter.x)
+  //       .attr("y1",currentStartCenter.y)
+  //       .attr("x2",currentEndCenter.x)
+  //       .attr("y2",currentEndCenter.y)
+  //       .attr("stroke","red")
+  //       .attr("stroke-width",2)
+  //       .attr("marker-start","url(#arrow)")
+  //       .attr("marker-end","url(#arrow)")
+  //
+  //   }
+  // }
+
   function drawNode(svg,nodeInfo){
     console.log('nodeInfo:');
     console.log(nodeInfo);
 
-    console.log(nodeInfo.nodetype);
+    console.log(nodeInfo.position.nodetype);
 
-    if(nodeInfo.nodetype === "first"){
+    if(nodeInfo.position.nodetype === "task"){
       svg.append("circle")
-        .attr("cx", nodeInfo.x)
-        .attr("cy", nodeInfo.y)
+        .attr('id',nodeInfo.id)
+        .attr("cx", nodeInfo.position.x)
+        .attr("cy", nodeInfo.position.y)
         .attr("r", 50)
         .attr('fill','#cc3311')
+        .on('click',function(){
+          clickedOnGraphObj(nodeInfo);
+        })
         .call(dragCircle)
         .append('text')
         .attr('x',10)
@@ -137,21 +294,33 @@ angular.module('myApp.view1', ['ngRoute'])
         .attr('height',30)
         .attr('font-size','8pt')
         .attr('fill','black')
-        .text('Circle');
+        .text('Circle')
 
-    } else if(nodeInfo.nodetype === "second") {
+    } else if(nodeInfo.position.nodetype === "input") {
       svg.append('rect')
-        .attr('x',nodeInfo.x-30)
-        .attr('y',nodeInfo.y-40)
+        .attr('x',nodeInfo.position.x-30)
+        .attr('y',nodeInfo.position.y-40)
         .attr('width','60px')
         .attr('height','80px')
         .attr('fill','#aa00ee')
+        .on('click',function(){
+          clickedOnGraphObj(nodeInfo);
+        })
         .call(dragSecond);
 
-    } else if(nodeInfo.nodetype === "third") {
+    } else if(nodeInfo.position.nodetype === "label") {
       svg.append('rect')
-        .attr('x',nodeInfo.x-30)
-        .attr('y',nodeInfo.y-40)
+        .attr('x',nodeInfo.position.x-30)
+        .attr('y',nodeInfo.position.y-40)
+        .attr('width','60px')
+        .attr('height','80px')
+        .attr('fill','#11bb22')
+        .call(dragThird);
+
+    }else if(nodeInfo.position.nodetype === "event") {
+      svg.append('rect')
+        .attr('x',nodeInfo.position.x-30)
+        .attr('y',nodeInfo.position.y-40)
         .attr('width','60px')
         .attr('height','80px')
         .attr('fill','#11bb22')
---
+++ backup/test1.js
@@ -0,0 +1,76 @@
+console.log(d3.event);
+// console.log(d3.event.layerY);
+
+// var line = svg.append("line")
+//   .attr("x1",50)
+//   .attr("y1",50)
+//   .attr("x2",200)
+//   .attr("y2",400)
+//   .attr("stroke","red")
+//   .attr("stroke-width",2)
+//   .attr("marker-start","url(#arrow)")
+//   .attr("marker-end","url(#arrow)")
+
+
+//
+// var arrow_path = "M2,2 L10,6 L2,10 L6,6 L2,2";
+// arrowMarker.append("path")
+//   .attr("d",arrow_path)
+//   .attr("fill","#000")
+// //绘制直线
+// var line = svg.append("line")
+//   .attr("x1",50)
+//   .attr("y1",50)
+//   .attr("x2",200)
+//   .attr("y2",400)
+//   .attr("stroke","red")
+//   .attr("stroke-width",2)
+//   .attr("marker-start","url(#arrow)")
+//   .attr("marker-end","url(#arrow)")
+
+
+
+//添加defs标签
+var defs = svg.append("defs");
+
+
+
+//添加marker标签及其属性
+var arrowMarker = defs.append("marker")
+  .attr("id","arrow")
+  .attr("markerUnits","strokeWidth")
+  .attr("markerWidth",12)
+  .attr("markerHeight",12)
+  .attr("viewBox","0 0 12 12")
+  .attr("refX",6)
+  .attr("refY",6)
+  .attr("orient","auto");
+
+
+
+// shouldDraw = false;
+// if(currentStartCenter && !currentEndCenter){
+//   currentEndCenter = {
+//     'x': nodeInfo.x,
+//     'y': nodeInfo.y,
+//     'type':'second'
+//   };
+//   shouldDraw = false;
+// }
+//
+// drawFinalLine();
+
+
+.on('mouseover',function(){
+  console.log('mouseover...');
+  shouldDraw = false;
+
+
+})
+  .on('mouseout',function(){
+
+    if(!currentEndCenter){
+      shouldDraw = true;
+    }
+
+  })
